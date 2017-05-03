7e2a15888456ee7e2a0be075fbf0359b604e4692
Xiao-Ou Zhang <kepbod@gmail.com>
2017-05-02 22:33:07 -0400

    annotated promoters

--- rampage/call_peak.py
+++ rampage/call_peak.py
@@ -9,7 +9,7 @@ Options:
     -g GTF --gtf=GTF               Assembled gene annotation GTF file.
     --db DB                        Assembled gene annotation database.
     -p THREAD --thread=THREAD      Threads. [default: 5]
-    --promoter=PROMOTER            Promoter region. [default: 2000]
+    --promoter=PROMOTER            Promoter region. [default: 1000]
 '''
 
 import math
@@ -60,10 +60,17 @@ def call_peak(options):
     for gene in db.features_of_type('gene'):
         gene_info = '%s\t%s\t%d\t%d\t%s' % (gene.id, gene.seqid, gene.start,
                                             gene.end, gene.strand)
+        gpromoter = []
+        for t in db.children(gene.id, featuretype='transcript'):
+            if gene.strand == '+':
+                gpromoter.append([t.start - pregion, t.start + pregion])
+            else:
+                gpromoter.append([t.end - pregion, t.end + pregion])
+        gpromoter = Interval(gpromoter)
         p5, p3 = peak5[gene.strand], peak3[gene.strand]
         results.append(p.apply_async(call_peak_for_gene,
                                      args=(rampage, p5, p3, gene_info,
-                                           pregion,)))
+                                           gpromoter, pregion,)))
     p.close()
     p.join()
     peaks, pvalue = [], []
@@ -83,13 +90,13 @@ def call_peak(options):
             outf.write('%s\t%f\n' % (p, q))
 
 
-def call_peak_for_gene(rampage, peak5, peak3, gene_info, pregion):
+def call_peak_for_gene(rampage, peak5, peak3, gene_info, gpromoter, pregion):
     gene_id, gene_chrom, gene_start, gene_end, gene_strand = gene_info.split()
     gene_start = int(gene_start)
     gene_end = int(gene_end)
     peak_loc = assign_peak(rampage, gene_chrom, gene_start, gene_end,
                            gene_strand, pregion)
-    peaks = fetch_peak(peak5, peak_loc, gene_chrom)
+    peaks = fetch_peak(peak5, peak_loc, gene_chrom, gpromoter)
     e_mean, var = cal_expression(peak3, gene_chrom, gene_start, gene_end)
     if e_mean == 0:
         return None, None
@@ -124,13 +131,17 @@ def assign_peak(rampage, g_chrom, g_start, g_end, g_strand, pregion):
     return peak_loc
 
 
-def fetch_peak(peak5, peak_loc, chrom):
+def fetch_peak(peak5, peak_loc, chrom, gpromoter):
     peaks = set()
     peak5f = pysam.TabixFile(peak5)
     for loc in Interval(peak_loc):
         start, end = loc[:2]
+        if [start, end] in gpromoter:
+            pflag = 'Yes'
+        else:
+            pflag = 'No'
         for p in peak5f.fetch(chrom, start, end):
-            peaks.add(p)
+            peaks.add(p + '\t' + pflag)
     return peaks
 
 
@@ -150,10 +161,10 @@ def cal_expression(peak3, g_chrom, g_start, g_end):
 def filter_peak(peaks, e_mean, var):
     filtered_peaks = []
     for p in peaks:
-        chrom, start, end, _, value = p.split()
+        chrom, start, end, _, value, pflag = p.split()
         value = float(value)
         z_score, p_value = wald_test(value, e_mean, var)
-        filtered_peaks.append(['\t'.join([chrom, start, end, str(value),
+        filtered_peaks.append(['\t'.join([chrom, start, end, pflag, str(value),
                                           str(e_mean), str(z_score),
                                           str(p_value)]),
                                p_value])
