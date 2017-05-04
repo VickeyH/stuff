5a61b3936bc7c0853865103e47762cc673616a62
Xiao-Ou Zhang <kepbod@gmail.com>
2017-05-04 16:57:18 -0400

    add minimum read count setting

--- rampage/rm_pcr.py
+++ rampage/rm_pcr.py
@@ -8,10 +8,12 @@ Options:
     --version                      Show version.
     -p THREAD --thread=THREAD      Threads. [default: 5]
     -o OUTPUT --output=OUTPUT      Output directory. [default: rampage_peak]
+    --min=MIN                      Minimum read counts. [default: 3]
 '''
 
 import os
 import os.path
+from collections import defaultdict
 from seqlib.path import create_dir
 from seqlib.ngs import check_bam
 from seqlib.seq import dna_to_rna
@@ -28,6 +30,7 @@ def rm_pcr(options):
     rampage = options['<rampage>']
     thread = int(options['--thread'])
     folder = create_dir(options['--output'])
+    min_read = int(options['--min'])
     # remove PCR duplicates
     if thread > 1:  # more than 1 thread, parse bam file based on chromosome
         from multiprocessing import Pool
@@ -45,10 +48,10 @@ def rm_pcr(options):
             else:
                 init = False
             collapsed_pairs = r.get()
-            write_signal(collapsed_pairs, folder, init)
+            write_signal(collapsed_pairs, folder, min_read, init)
     else:
         collapsed_pairs = remove_pcr(rampage)
-        write_signal(collapsed_pairs, folder, True)
+        write_signal(collapsed_pairs, folder, min_read, True)
 
 
 def check_file(fname, init):
@@ -57,37 +60,48 @@ def check_file(fname, init):
     return open(fname, 'a')
 
 
-def write_signal(pairs, folder, init):
+def write_signal(pairs, folder, min_read, init):
     bed5p = check_file(folder + '/rampage_plus_5end.bed', init)
     bed5m = check_file(folder + '/rampage_minus_5end.bed', init)
     bed3p = check_file(folder + '/rampage_plus_3read.bed', init)
     bed3m = check_file(folder + '/rampage_minus_3read.bed', init)
     bed = check_file(folder + '/rampage_link.bed', init)
+    uniq_pairs = defaultdict(int)
     for pair in pairs:
+        info = pair.rsplit('\t', 1)[0]
+        uniq_pairs[info] += 1
+    for pair in uniq_pairs:
+        read_count = uniq_pairs[pair]
+        if read_count < min_read:
+            continue
         pair_info = pair.split()
-        r1_chrom, r1_start, r1_end, r1_strand = pair_info[:4]
-        r2_chrom, r2_start, r2_end, r2_strand = pair_info[5:9]
-        if r1_strand == '+':
+        r1_chrom, r1_start, r1_end, strand = pair_info[:4]
+        r2_chrom, r2_start, r2_end = pair_info[4:]
+        if strand == '+':
             start = int(r1_start)
             end = int(r2_end)
             bed5p.write('%s\t%d\t%d\t5end\t0\t%s\n' % (r1_chrom, start,
-                                                       start + 1, r1_strand))
+                                                       start + 1, strand) *
+                        read_count)
             bed3p.write('%s\t%d\t%d\t3read\t0\t%s\n' % (r2_chrom, end - 1,
-                                                        end, r2_strand))
+                                                        end, strand) *
+                        read_count)
             offset = '0,' + str(end - start - 1)
             bed.write('\t'.join([r1_chrom, r1_start, r2_end, 'link\t0',
-                                 r1_strand, r1_start, r1_start, '0,0,0',
+                                 strand, r1_start, r1_start, '0,0,0',
                                  '2', '1,1', offset]) + '\n')
         else:
             start = int(r2_start)
             end = int(r1_end)
             bed5m.write('%s\t%d\t%d\t5end\t0\t%s\n' % (r1_chrom, end - 1,
-                                                       end, r1_strand))
+                                                       end, strand) *
+                        read_count)
             bed3m.write('%s\t%d\t%d\t3read\t0\t%s\n' % (r2_chrom, start,
-                                                        start - 1, r2_strand))
+                                                        start - 1, strand) *
+                        read_count)
             offset = '0,' + str(end - start - 1)
             bed.write('\t'.join([r1_chrom, r2_start, r1_end, 'link\t0',
-                                 r1_strand, r2_start, r2_start, '0,0,0',
+                                 strand, r2_start, r2_start, '0,0,0',
                                  '2', '1,1', offset]) + '\n')
 
 
@@ -146,8 +160,10 @@ def fetch_read2(bam, read1, chrom):
         else:
             barcode = dna_to_rna(read.query_sequence[-15:],
                                  strand=strand)
-        collapsed_pairs.add('\t'.join(read1[read_id][1:] + [chrom, start, end,
-                                                            strand, barcode]))
+        read1_barcode = read1[read_id][5]
+        collapsed_pairs.add('\t'.join(read1[read_id][1:5] + [chrom, start, end,
+                                                             read1_barcode +
+                                                             barcode]))
     return collapsed_pairs
 
 
